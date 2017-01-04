386163c88e8696706edb4d6e71dc5aaa3ef7dbf7
Zawar Shahid <zawar_shahid93@hotmail.com>
2017-01-05 00:17:16 +0500

    Assignment-1 Solution Completed.

--- module1 - solution/app.js
+++ module1 - solution/app.js
@@ -1,19 +1,64 @@
-(function(){
-  'use strict'
-  angular.module("LunchCheck", [])
-  .controller("LunchCheckController", ['$scope', function($scope){
-    $scope.info = "";
-    $scope.meals = "";
-    $scope.appState = true;
-    $scope.checkMeals = function(){
-      var meal = [];
-      if($scope.meals == "")
-      {
-        $scope.info = "Please enter data first.";
-      }
-      else {
-        meal = $scope.meals.split(",");
-      }
-    }
-  }]);
+(function() {
+    'use strict'
+    angular.module("LunchCheck", [])
+        .controller("LunchCheckController", ['$scope', function($scope) {
+            $scope.info = "";
+            $scope.meals = "";
+            $scope.appState = true;
+            $scope.comment  = "";
+            var commentFlag = false;
+
+            $scope.checkMeals = function() {
+                var meal = [];
+                if ($scope.meals == "") {
+                    $scope.col = {
+                        "color": "red",
+                        "font-size": "25px"
+                    };
+                    $scope.mealInput = {
+                      "border-color": "red",
+                      "border-style": "solid",
+                      "border-width": "3px"
+                    };
+                    $scope.info = "Please enter data first.";
+                } else {
+                    meal = $scope.meals.split(",");
+                    for(var i=0; i<meal.length; i++){
+                      if(meal[i] == ""){
+                        meal.splice(i);
+                        this.commentFlag = true;
+                      }
+                    }
+                    if (meal.length > 3) {
+                        $scope.col = {
+                            "color": "green",
+                            "font-size": "25px"
+                        };
+                        $scope.mealInput = {
+                          "border-color": "green",
+                          "border-style": "solid",
+                          "border-width": "4px"
+                        };
+                        $scope.info = "Too Much!";
+                        if(this.commentFlag == true){
+                          $scope.comment = "You do NOT consider an empty item, i.e.`, ,`";
+                        }
+                    } else {
+                        $scope.col = {
+                            "color": "green",
+                            "font-size": "25px"
+                        };
+                        $scope.mealInput = {
+                          "border-color": "green",
+                          "border-style": "solid",
+                          "border-width": "4px"
+                        };
+                        $scope.info = "Enjoy!";
+                        if(this.commentFlag == true){
+                          $scope.comment = "You do NOT consider an empty item, i.e. , ,";
+                        }
+                    }
+                }
+            }
+        }]);
 })();
